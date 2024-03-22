ec63f185f80cc17933193d6059e38ce169a3d9fa
warmachine028 <warmachine028@outlook.com>
2024-03-22 18:26:46 +0530

    feat: added footer

--- src/Sections/Work.jsx
+++ src/Sections/Work.jsx
@@ -1,34 +1,35 @@
-
 const Work = () => {
-  return (
-    <section className="">
-      <h1 className="text-6xl font-bold font-body pl-1leading-[66px] tracking-[0.5px] flex flex-">RELEVANT <br/>
-        EXPERIENCE
-      </h1>
-      <div className="flex flex-col gap-4">
-        <div className="bg-white border-2  w-[700px] h-[150px] shadow-black-100 ">
-          <h4>Crosby</h4>
-          <h5><strong>xxxxxxxxxxxxxx</strong></h5>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Libero qui excepturi perspiciatis magnam voluptatum Libero qui excepturi perspiciatis magnam voluptatum </p>
-        </div>
-      <div className="bg-white border-2  w-[700px] h-[150px] shadow-black-100 ">
-          <h4>Crosby</h4>
-           <h5><strong>xxxxxxxxxxxxxx</strong></h5>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Libero qui excepturi perspiciatis magnam voluptatum Libero qui excepturi perspiciatis magnam voluptatum </p>
-        </div>
-        <div className="bg-white border-2  w-[700px] h-[150px] shadow-black-100 ">
-          <h4>Crosby</h4>
-           <h5><strong>xxxxxxxxxxxxxx</strong></h5>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Libero qui excepturi perspiciatis magnam voluptatum Libero qui excepturi perspiciatis magnam voluptatum </p>
-        </div>
-      
-        </div>
-    </section>
-    
-  )
+	return (
+		<section style={{}} className="flex flex-col gap-12  text-balance max-w-screen-xl mx-auto font-semibold font-body mb-[110px] w-full">
+			<h1 className="text-6xl font-bold font-body pl-1leading-[66px] tracking-[0.5px] flex">
+				RELEVANT <br />
+				EXPERIENCE
+			</h1>
+			<div className="flex flex-col gap-4 self-center">
+				<div className="bg-white shadow-md p-2 flex flex-col w-[700px] h-[150px] ">
+					<h4 className="text-start">Crosby</h4>
+					<h5>
+						<strong>Lorem ipsum dolor </strong>
+					</h5>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero qui excepturi perspiciatis magnam voluptatum </p>
+				</div>
+				<div className="bg-white shadow-md p-2 flex flex-col w-[700px] h-[150px] ">
+					<h4 className="text-start">Crosby</h4>
+					<h5>
+						<strong>Lorem ipsum dolor </strong>
+					</h5>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero qui excepturi perspiciatis magnam voluptatum </p>
+				</div>
+				<div className="bg-white shadow-md p-2 flex flex-col w-[700px] h-[150px] ">
+					<h4 className="text-start">Crosby</h4>
+					<h5>
+						<strong>Lorem ipsum dolor </strong>
+					</h5>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero qui excepturi perspiciatis magnam voluptatum </p>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Work
